3a9eb265b9efe06b4ac6f9a8662c3f1d94fc0853
Callum Dixon <callum.dixon002@gmail.com>
2022-02-11 16:30:16 +0000

    gradle upgrade and color theme change

--- app/src/main/java/com/example/kennings_android_native/views/AccountView.kt
+++ app/src/main/java/com/example/kennings_android_native/views/AccountView.kt
@@ -1,10 +1,35 @@
 package com.example.kennings_android_native.views
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavController
 
 @Composable
 fun AccountView(navController: NavController) {
-    Text("Account")
+    Scaffold( topBar = { TopAppBar { Text("Account") } } ) {
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.LightGray),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Account",
+                fontSize = MaterialTheme.typography.h3.fontSize,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+        }
+    }
 }
--- app/src/main/java/com/example/kennings_android_native/views/HomeView.kt
+++ app/src/main/java/com/example/kennings_android_native/views/HomeView.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,18 +16,22 @@ import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun HomeView() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.LightGray),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = "Home",
-            fontSize = MaterialTheme.typography.h3.fontSize,
-            fontWeight = FontWeight.Bold,
-            color = Color.White
-        )
+
+    Scaffold( topBar = { TopAppBar { Text("Home") } } ) {
+        
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.LightGray),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Home",
+                fontSize = MaterialTheme.typography.h3.fontSize,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+        }
     }
 }
 
